1f13508769a09b898ac5da0ad1bcfc7753ebcdc8
indspug <indspug@github.com>
2018-01-17 22:45:48 +0900

    重みの初期値比較まで済み

--- 06.techniques/common/optimizer.py
+++ 06.techniques/common/optimizer.py
@@ -41,3 +41,60 @@ class Momentum:
 			params[key] += self.v[key]
 	
 
+##################################################
+# AdamGrad
+##################################################
+class AdamGrad:
+	
+	# コンストラクタ
+	def __init__(self, lr=0.01):
+		self.lr = lr
+		self.h = None
+	
+	# パラメータ更新
+	def update(self, params, grads):
+		if self.h is None:
+			self.h = {}
+			for key,val in params.items():
+				self.h[key] = np.zeros_like(val)
+		
+		for key in params.keys():
+			self.h[key] += grads[key] * grads[key]
+			params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+		
+
+##################################################
+# Adam
+##################################################
+class Adam:
+	
+	"""Adam (http://arxiv.org/abs/1412.6980v8)"""
+	
+	# コンストラクタ
+	def __init__(self, lr=0.001, beta1=0.9, beta2=0.99):
+		self.lr = lr
+		self.beta1 = beta1
+		self.beta2 = beta2
+		self.iter = 0
+		self.m = None
+		self.v = None
+	
+	# パラメータ更新
+	def update(self, params, grads):
+		if self.m is None:
+			self.m, self.v = {}, {}
+			for key, val in params.items():
+				self.m[key] = np.zeros_like(val)
+				self.v[key] = np.zeros_like(val)
+				
+		self.iter += 1
+		lr_t = self.lr * np.sqrt(1.0 - self.beta2 ** self.iter) / \
+								(1.0 - self.beta1 ** self.iter)
+		
+		for key in params.keys():
+			self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+			self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+			params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
+			
+	
+
--- 06.techniques/train_mnist.py
+++ 06.techniques/train_mnist.py
@@ -21,6 +21,19 @@ def label_to_one_hot(t, label_num):
 	
 	return t1
 	
+
+##################################################
+# 損失関数のグラフを滑らかにするために用いる
+##################################################
+def smooth_curve(x):
+	"""参考：http://glowingpython.blogspot.jp/2012/02/convolution-with-numpy.html
+	"""
+	window_len = 11
+	s = np.r_[x[window_len-1:0:-1], x, x[-1:-window_len:-1]]
+	w = np.kaiser(window_len, 2)
+	y = np.convolve(w/w.sum(), s, mode='valid')
+	return y[5:len(y)-5]
+
 ##################################################
 # メイン
 ##################################################
@@ -35,9 +48,9 @@ if __name__ == '__main__':
 	t_test = label_to_one_hot(t_test, 10)
 	
 	# ハイパーパラメータ
-	iter_num = 10000			# 繰返し計算回数
+	iter_num = 20000			# 繰返し計算回数
 	batch_size = 100			# バッチサイズ
-	learning_rate = 0.1			# 学習係数
+	learning_rate = 0.01			# 学習係数
 	
 	# パラメータ設定
 	train_size = x_train.shape[0]	# 学習データ数
@@ -46,11 +59,14 @@ if __name__ == '__main__':
 	
 	# 誤差逆伝播の最適化法取得
 	optimizers = {}
-	optimizers['SGD'] = SGD()
-	optimizers['Momentum'] = Momentum()
+	optimizers['SGD'] = SGD(learning_rate)
+	optimizers['Momentum'] = Momentum(learning_rate)
+	optimizers['AdamGrad'] = AdamGrad(learning_rate)
+	optimizers['Adam'] = Adam(learning_rate)
 	
 	network = {}
 	train_loss = {}
+	epoch_num = 0
 	for key in optimizers.keys():
 		# 2層のニューラルネットワーク取得
 		network[key] = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
@@ -73,30 +89,37 @@ if __name__ == '__main__':
 			optimizers[key].update(network[key].params, grads)
 			
 			# 学習経過の記録
-			loss = network[key].loss(x_batch, t_batch)
-			train_loss[key].append(loss)
-		
-		# 1エポック毎に損失関数の数値を表示する
+			#loss = network[key].loss(x_batch, t_batch)
+			
+		# 1エポック毎に損失関数の数値を表示し、
 		if (i % iter_per_epoch) == 0:
 			epoch = i / iter_per_epoch
 			print('epoch : %04d' % epoch)
+			epoch_num += 1
 			for key in optimizers.keys():
 				loss = network[key].loss(x_batch, t_batch)
+				train_loss[key].append(loss)
 				print('%10s : loss=%f' % (key, loss))
 	
 	fig = plt.figure()
 	
 	# 損失関数の経過表示
 	loss_plot = fig.add_subplot(1, 1, 1)
-	markers = {'SGD':'o', 'Momentum':'x'}
-	iteration = np.arange(iter_num)
+	markers = {'SGD':'o', 'Momentum':'x', "AdamGrad":"s", "Adam":"D"}
+	#iteration = np.arange(iter_num)
+	epoch = np.arange(0, epoch_num)
 	for key in optimizers.keys():
-		loss_plot.plot(iteration, train_loss[key], label='loss', marker=markers[key])
+		#loss_plot.plot(iteration, train_loss[key], label=key, marker=markers[key], markevery=100)
+		loss_plot.plot(epoch, train_loss[key], \
+						label=key, marker=markers[key])
 		
-	loss_plot.set_xlim(0, iter_num)
-	loss_plot.set_ylim(0, 1.0)
-	loss_plot.set_xlabel('Iteration')
+	#loss_plot.set_xlim(0, iter_num)
+	loss_plot.set_xlim(0, epoch_num)
+	loss_plot.set_ylim(0, 0.5)
+	#loss_plot.set_xlabel('Iteration')
+	loss_plot.set_xlabel('Epoch')
 	loss_plot.set_ylabel('Loss')
+	loss_plot.legend()
 	
 	plt.show()
 	
